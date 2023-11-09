f1d88871cca9a581975db49151df4b7bd04f0348
zockerhuhn <fbxfbxfbx5@gmail.com>
2023-11-09 19:53:44 +0100

    Reflektionssensor

--- programs/Main/Main.ino
+++ programs/Main/Main.ino
@@ -6,12 +6,12 @@
  *     ... das richtige Arduino Board eingestellt ist
  *     ... das richtige "Sketch File" ausgewählt ist (das ändert sich nämlich nicht automatisch)
  *     ... die richtige C/C++ Konfiguration eingestellt ist (sonst gibt es noch mehr "rote swiggels")
- * 
+ *
  * :: Externen RGB Farbsensor auslesen ::
  * :: Serial Plotter ausprobieren ::
  * :: Hauptprogramm-Schleife in Zustände unterteilen ::
  * :: Programm in Funktionen unterteilen ::
- * 
+ *
  * Hardware-Aufbau:
  * TCS34725:    Arduino Due / Arduino Nano RP2040 Connect:
  *     [LED <-- (kann man optional an irgendeinen Digital-Pin oder den INT Pin vom Sensor anschließen,
@@ -23,11 +23,11 @@
  *     [3V3 --> (2,3V Spannungsversorgung, falls man sowas irgendwo benötigt)]
  *      GND <-- GND
  *      VIN <-- 3V3
- * 
+ *
  * Der VL53L0X ist ein externer Sensor, kann also entweder
  * - an   Bus I2C0 ("Wire")
  * - oder Bus I2C1 ("Wire1") verbunden werden.
-*/
+ */
 
 /** der I2C Bus */
 #include <Wire.h>
@@ -39,48 +39,38 @@
 Adafruit_TCS34725 rgbSensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
 /** optional: Stoppuhr, um zu Verbindungsverluste zu erkennen */
-#include <Chrono.h> 
+#include <Chrono.h>
 Chrono helligkeitStatischStoppuhr = Chrono(Chrono::MILLIS, false); // noch nicht gestartet
 
-enum Modus {
-    /* Werte im Serial Monitor anzeigen. */
-    WERTE_LOGGEN,
-    /* Geht nur in der offiziellen Arduino IDE: Komma-Separierte Liste für den Serial Plotter. */
-    IM_SERIAL_PLOTTER_ZEIGEN,
-};
-
-/** hier einstellen, was das Programm mit den Sensorwerten anfangen soll: */
-Modus modus = WERTE_LOGGEN;
-
-#include "ZumoReflectanceSensorArray.h"
+#include <QTRSensors.h>
 #include "ZumoMotors.h"
-#include <Wire.h>
 
 ZumoMotors motoren;
-ZumoReflectanceSensorArray reflektionsSensoren;
 
 int varrechts = 0;
 int varlinks = 0;
-unsigned int sensorwerte[6];
-
+QTRSensors sensorLeiste = QTRSensors();
+const uint8_t SENSOR_LEISTE_ANZAHL_SENSOREN = 6;
+const uint8_t SENSOR_LEISTE_PINS[] = {10, 11, 12, 9, 8, 7};
 
 void setup()
 {
   Serial.begin(115200);
-
   // I2C Bus 1x für alle Bus-Teilnehmer initialisieren (sonst crasht das Betriebssystem)
-  Wire.begin(); // Bus I2C0
+  Wire.begin();           // Bus I2C0
   Wire.setClock(1000000); // 1MHz Kommunikationsgeschwindigkeit
-  //Wire1.begin();  // Bus I2C1
-  // hier den zu nutzenden I2C Bus einstellen:
-  if (!rgbSensor.begin(TCS34725_ADDRESS, &Wire)) {
+  // Wire1.begin();  // Bus I2C1
+  //  hier den zu nutzenden I2C Bus einstellen:
+  if (!rgbSensor.begin(TCS34725_ADDRESS, &Wire))
+  {
     delay(10000); // damit wir Zeit haben den Serial Monitor zu öffnen nach dem Upload
-    Serial.println("RGB Farbsensor Verdrahtung prüfen! Programm Ende.");
-    while (1);
+    Serial.println("RGB Farbsensor Verdrahtung prüfen!");
   }
   helligkeitStatischStoppuhr.start();
   Serial.println("Initialisierung Farbe 1 abgeschlossen");
-  reflektionsSensoren.init();  // Library initialisieren
+  sensorLeiste.setTypeRC();
+  sensorLeiste.setSensorPins(SENSOR_LEISTE_PINS, SENSOR_LEISTE_ANZAHL_SENSOREN);
+  Serial.println("Initialisierung Reflektionssensor abgeschlossen");
   motoren.flipLeftMotor(true);
   motoren.flipRightMotor(true);
 }
@@ -89,23 +79,6 @@ void setup()
 // Roh-Werte (Es gibt auch kalibierte Werte, aber die sind sehr langsam auszulesen):
 uint16_t rot, gruen, blau, helligkeit;
 uint16_t rot2, gruen2, blau2, helligkeit2;
-
-void read_reflectionandprint()
-{
-  reflektionsSensoren.read(sensorwerte);
-  Serial.print(sensorwerte[0]); // Sensor links
-  Serial.print("\t");           // Tab
-  Serial.print(sensorwerte[1]); // Sensor fast links
-  Serial.print("\t");           // Tab
-  Serial.print(sensorwerte[2]); // Sensor links mittig
-  Serial.print("\t");           // Tab
-  Serial.print(sensorwerte[3]); // Sensor rechts mittig
-  Serial.print("\t");           // Tab
-  Serial.print(sensorwerte[4]); // Sensor fast rechts
-  Serial.print("\t");           // Tab
-  Serial.print(sensorwerte[5]); // Sensor rechts
-  Serial.print("\n");           // Neue Zeile
-}
 
 void doppelschwarz()
 {
@@ -127,10 +100,10 @@ void doppelschwarz()
       delay(1250);
       right();
       delay(1000);
-      reflektionsSensoren.read(sensorwerte);
-      while (sensorwerte[5] <= 120)
+      read_reflectionandprint();
+      while (helligkeiten[5] <= 120)
       {
-        reflektionsSensoren.read(sensorwerte);
+        read_reflectionandprint();
       }
     }
   }
@@ -142,10 +115,10 @@ void doppelschwarz()
       delay(1250);
       left();
       delay(1000);
-      reflektionsSensoren.read(sensorwerte);
-      while (sensorwerte[1] <= 120)
+      read_reflectionandprint();
+      while (helligkeiten[1] <= 120)
       {
-        reflektionsSensoren.read(sensorwerte);
+        read_reflectionandprint();
       }
     }
     else
@@ -153,8 +126,8 @@ void doppelschwarz()
       straight();
       delay(1200);
       motoren.setSpeeds(0, 0);
-      reflektionsSensoren.read(sensorwerte);
-      if (sensorwerte[0] >= 120 || sensorwerte[1] >= 120 || sensorwerte[2] >= 120 || sensorwerte[3] >= 120 || sensorwerte[4] >= 120 || sensorwerte[5] >= 120)
+      read_reflectionandprint();
+      if (helligkeiten[0] >= 120 || helligkeiten[1] >= 120 || helligkeiten[2] >= 120 || helligkeiten[3] >= 120 || helligkeiten[4] >= 120 || helligkeiten[5] >= 120)
       {
         // not else lol
       }
@@ -163,10 +136,10 @@ void doppelschwarz()
         left();
         delay(2500);
         right();
-        reflektionsSensoren.read(sensorwerte);
-        while (sensorwerte[1] <= 120 && sensorwerte[2] <= 120 && sensorwerte[3] <= 120 && sensorwerte[4] <= 120)
+        read_reflectionandprint();
+        while (helligkeiten[1] <= 120 && helligkeiten[2] <= 120 && helligkeiten[3] <= 120 && helligkeiten[4] <= 120)
         {
-          reflektionsSensoren.read(sensorwerte);
+          read_reflectionandprint();
           Serial.print("\n");
           Serial.print("suche...");
         }
@@ -178,11 +151,11 @@ void doppelschwarz()
 void loop()
 {
   read_reflectionandprint();
-  if ((sensorwerte[0] >= 120) && (sensorwerte[5] >= 120))
+  if ((helligkeiten[0] >= 120) && (helligkeiten[5] >= 120))
   {
     doppelschwarz();
   }
-  else if ((sensorwerte[2] >= 120 || sensorwerte[3] >= 120) && (sensorwerte[0] <= 120 && sensorwerte[1] <= 120 && sensorwerte[4] <= 120 && sensorwerte[5] <= 120))
+  else if ((helligkeiten[2] >= 120 || helligkeiten[3] >= 120) && (helligkeiten[0] <= 120 && helligkeiten[1] <= 120 && helligkeiten[4] <= 120 && helligkeiten[5] <= 120))
   {
     Serial.print("\n");
     Serial.print("Linie");
@@ -196,7 +169,7 @@ void loop()
       varlinks = varlinks - 5;
     }
   }
-  else if (sensorwerte[0] >= 120 || sensorwerte[1] >= 120)
+  else if (helligkeiten[0] >= 120 || helligkeiten[1] >= 120)
   {
     Serial.print("\n");
     Serial.print("links");
@@ -209,7 +182,7 @@ void loop()
       doppelschwarz();
     }
   }
-  else if (sensorwerte[4] >= 120 || sensorwerte[5] >= 120)
+  else if (helligkeiten[4] >= 120 || helligkeiten[5] >= 120)
   {
     Serial.print("\n");
     Serial.print("rechts");
@@ -222,7 +195,7 @@ void loop()
       doppelschwarz();
     }
   }
-  else if (sensorwerte[0] <= 120 && sensorwerte[1] <= 120 && sensorwerte[2] <= 120 && sensorwerte[3] <= 120 && sensorwerte[4] <= 120 && sensorwerte[5] <= 120)
+  else if (helligkeiten[0] <= 120 && helligkeiten[1] <= 120 && helligkeiten[2] <= 120 && helligkeiten[3] <= 120 && helligkeiten[4] <= 120 && helligkeiten[5] <= 120)
   {
     Serial.print("\n");
     Serial.print("keine Linie...");
@@ -261,78 +234,91 @@ const uint16_t VERBINDUNG_VERLOREN = 0;
 uint16_t vorheriges_rot, vorheriges_gruen, vorheriges_blau, vorherige_helligkeit = VERBINDUNG_VERLOREN;
 uint16_t vorheriges_rot2, vorheriges_gruen2, vorheriges_blau2, vorherige_helligkeit2 = VERBINDUNG_VERLOREN;
 
-void readColor() {
-    rgbSensor.getRawData(&rot, &gruen, &blau, &helligkeit);
-    /** Dieser Mechanismus hier ist gefährlich, wenn es passieren kann, dass die Sensoren lange Zeit das selbe sehen:
-     *  In meinen Versuchen habe ich oben den Gain von 4x auf 16x gestellt, um mehr Rauschen zu bekommen.
-     *  Mit Timeout 5s sehe ich keine False-Negatives mehr: */
-    if (!helligkeitStatischStoppuhr.hasPassed(5000)) {
-        // alles OK
-        if (vorheriges_rot != rot || vorheriges_gruen != gruen ||
-            vorheriges_blau != blau || vorherige_helligkeit != helligkeit) {
-            // merken: der Wert hat sich verändert
-            vorheriges_rot = rot;
-            vorheriges_gruen = gruen;
-            vorheriges_blau = blau;
-            vorherige_helligkeit = helligkeit;
-            helligkeitStatischStoppuhr.restart();
-        }
-        return; // rausgehen aus der Funktion, damit wir nicht zum Fehler kommen
-    }
-
-    // Fehler:
-    rot = VERBINDUNG_VERLOREN;
-    gruen = VERBINDUNG_VERLOREN;
-    blau = VERBINDUNG_VERLOREN;
-    helligkeit = VERBINDUNG_VERLOREN;
-    helligkeitStatischStoppuhr.restart(); // um bei Wackelkontakt Wiederverbindung zu erlauben
-    Serial.println("RGB Sensor 1 Verdrahtung prüfen!");
-}
-
-void readColor2() {
-    rgbSensor.getRawData(&rot2, &gruen2, &blau2, &helligkeit2);
-    /** Dieser Mechanismus hier ist gefährlich, wenn es passieren kann, dass die Sensoren lange Zeit das selbe sehen:
-     *  In meinen Versuchen habe ich oben den Gain von 4x auf 16x gestellt, um mehr Rauschen zu bekommen.
-     *  Mit Timeout 5s sehe ich keine False-Negatives mehr: */
-    if (!helligkeitStatischStoppuhr.hasPassed(5000)) {
-        // alles OK
-        if (vorheriges_rot2 != rot2 || vorheriges_gruen2 != gruen2 ||
-            vorheriges_blau2 != blau2 || vorherige_helligkeit2 != helligkeit2) {
-            // merken: der Wert hat sich verändert
-            vorheriges_rot2 = rot2;
-            vorheriges_gruen2 = gruen2;
-            vorheriges_blau2 = blau2;
-            vorherige_helligkeit2 = helligkeit2;
-            helligkeitStatischStoppuhr.restart();
-        }
-        return; // rausgehen aus der Funktion, damit wir nicht zum Fehler kommen
-    }
-
-    // Fehler:
-    rot2 = VERBINDUNG_VERLOREN;
-    gruen2 = VERBINDUNG_VERLOREN;
-    blau2 = VERBINDUNG_VERLOREN;
-    helligkeit2 = VERBINDUNG_VERLOREN;
-    helligkeitStatischStoppuhr.restart(); // um bei Wackelkontakt Wiederverbindung zu erlauben
-    Serial.println("RGB Sensor 2 Verdrahtung prüfen!");
-}
-
-void werteLoggen() {
-    Serial.println("R:" + String(rot) + " G:" + String(gruen) + " B:" + String(blau) + " C:" + String(helligkeit));
-}
-
-void fuerPlotterLoggen() {
-    Serial.println(String(blau) + "," + String(rot) + "," + String(gruen) + "," +
-                   String(helligkeit / 10)); // damit der Graph nicht die ganze Zeit raus und rein zoomt
-}
-
-void calculatecolor() {
-    if (((gruen >= blau) && (gruen >= rot)) && (gruen <= 1000) && (gruen >= 650))
+void readColor()
+{
+  rgbSensor.getRawData(&rot, &gruen, &blau, &helligkeit);
+  Serial.println("R:" + String(rot) + " G:" + String(gruen) + " B:" + String(blau) + " C:" + String(helligkeit));
+  /** Dieser Mechanismus hier ist gefährlich, wenn es passieren kann, dass die Sensoren lange Zeit das selbe sehen:
+   *  In meinen Versuchen habe ich oben den Gain von 4x auf 16x gestellt, um mehr Rauschen zu bekommen.
+   *  Mit Timeout 5s sehe ich keine False-Negatives mehr: */
+  if (!helligkeitStatischStoppuhr.hasPassed(5000))
+  {
+    // alles OK
+    if (vorheriges_rot != rot || vorheriges_gruen != gruen ||
+        vorheriges_blau != blau || vorherige_helligkeit != helligkeit)
     {
-        Serial.println("green");
+      // merken: der Wert hat sich verändert
+      vorheriges_rot = rot;
+      vorheriges_gruen = gruen;
+      vorheriges_blau = blau;
+      vorherige_helligkeit = helligkeit;
+      helligkeitStatischStoppuhr.restart();
     }
-    else {
-        Serial.println("not green");
+    return; // rausgehen aus der Funktion, damit wir nicht zum Fehler kommen
+  }
+
+  // Fehler:
+  rot = VERBINDUNG_VERLOREN;
+  gruen = VERBINDUNG_VERLOREN;
+  blau = VERBINDUNG_VERLOREN;
+  helligkeit = VERBINDUNG_VERLOREN;
+  helligkeitStatischStoppuhr.restart(); // um bei Wackelkontakt Wiederverbindung zu erlauben
+  Serial.println("RGB Sensor 1 Verdrahtung prüfen!");
+}
+
+void readColor2()
+{
+  rgbSensor.getRawData(&rot2, &gruen2, &blau2, &helligkeit2);
+  Serial.println("R:" + String(rot2) + " G:" + String(gruen2) + " B:" + String(blau2) + " C:" + String(helligkeit2));
+  /** Dieser Mechanismus hier ist gefährlich, wenn es passieren kann, dass die Sensoren lange Zeit das selbe sehen:
+   *  In meinen Versuchen habe ich oben den Gain von 4x auf 16x gestellt, um mehr Rauschen zu bekommen.
+   *  Mit Timeout 5s sehe ich keine False-Negatives mehr: */
+  if (!helligkeitStatischStoppuhr.hasPassed(5000))
+  {
+    // alles OK
+    if (vorheriges_rot2 != rot2 || vorheriges_gruen2 != gruen2 ||
+        vorheriges_blau2 != blau2 || vorherige_helligkeit2 != helligkeit2)
+    {
+      // merken: der Wert hat sich verändert
+      vorheriges_rot2 = rot2;
+      vorheriges_gruen2 = gruen2;
+      vorheriges_blau2 = blau2;
+      vorherige_helligkeit2 = helligkeit2;
+      helligkeitStatischStoppuhr.restart();
     }
-    
+    return; // rausgehen aus der Funktion, damit wir nicht zum Fehler kommen
+  }
+
+  // Fehler:
+  rot2 = VERBINDUNG_VERLOREN;
+  gruen2 = VERBINDUNG_VERLOREN;
+  blau2 = VERBINDUNG_VERLOREN;
+  helligkeit2 = VERBINDUNG_VERLOREN;
+  helligkeitStatischStoppuhr.restart(); // um bei Wackelkontakt Wiederverbindung zu erlauben
+  Serial.println("RGB Sensor 2 Verdrahtung prüfen!");
+}
+
+void calculatecolor()
+{
+  if (((gruen >= blau) && (gruen >= rot)) && (gruen <= 1000) && (gruen >= 650))
+  {
+    Serial.println("green");
+  }
+  else
+  {
+    Serial.println("not green");
+  }
+}
+
+// hier speichern wir die 6 Sensorwerte ab:
+uint16_t helligkeiten[SENSOR_LEISTE_ANZAHL_SENSOREN];
+
+void read_reflectionandprint()
+{
+  sensorLeiste.read(helligkeiten);
+  for (int i = 0; i < SENSOR_LEISTE_ANZAHL_SENSOREN; i++)
+  {
+    Serial.print(String(helligkeiten[i]) + '\t'); // alles in eine Zeile
+  }
+  Serial.println(); // neue Zeile beginnen
 }
