fafd0c36d05ca1a4cbd6c7a1f3bc2650845cdaa8
FlederJ <96187657+FlederJ@users.noreply.github.com>
2024-09-19 14:34:54 +0200

    updated kreuzung logic for when no green is detected, adjusted the time for kreuzung correctly

--- programs/Main/kreuzung.h
+++ programs/Main/kreuzung.h
@@ -10,25 +10,37 @@ void kreuzung(bool bothSides) {
     straight();
     bool green1 = false; // right
     bool green2 = false; // left
-
-    for (int i = 0; i < 20 /*adjust that value*/; i++) {
-      // TODO find out how much distance a kreuzung is and how much time this corresponds to for the motors
+    int reading_time = 6;  /*adjust that value*/
+    
+    for (int i = 0; i < reading_time; i++) {
       readColor2();
       readColor();
 
       if (calculateColor() && !green1) {
         green1 = true; 
         Serial.print("Found green 1 (right)\t");
-        i = 18; // Let the robot check for the other green just one more time
+        i = reading_time - 2; // Let the robot check for the other green just one more time
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(20);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(20);
+        digitalWrite(LED_BUILTIN, LOW);
       }
       if (calculateColor2() && !green2) {
         green2 = true;
         Serial.print("Found green 2 (left)\t");
-        i = 18;
+        i = reading_time - 2;
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(20);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(20);
+        digitalWrite(LED_BUILTIN, LOW);
       }
+
       if (green1 && green2) {
         break;
       }
+
       else if (green1 || green2) {
         // Stop to indicate that green has been detected
         digitalWrite(LED_BUILTIN, LOW);
@@ -42,6 +54,7 @@ void kreuzung(bool bothSides) {
     }
     digitalWrite(LED_BUILTIN, LOW);
 
+    // SOMEWHERE THERE IS A CASE WHERE THE ROBOT JUST STOPS!!!!!
 
     // Handle the recorded greens
     if (green1 && green2) {
@@ -68,6 +81,8 @@ void kreuzung(bool bothSides) {
     }
 
     else { // Did not find any green
+      straight();
+      delay(1500);
       // finding line
       left(90);
       
