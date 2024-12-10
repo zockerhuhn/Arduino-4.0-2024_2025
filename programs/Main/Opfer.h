d25b0aba99737ff7bb8903dafab00e4c0a37f473
zockerhuhn <fbxfbxfbx5@gmail.com>
2024-12-10 14:39:36 +0100

    basic straighten with direction basis

--- programs/Main/Opfer.h
+++ programs/Main/Opfer.h
@@ -1,3 +1,21 @@
-void opfer() {
+void Opfer() {
+    stop();
     ReadDirection();
+}
+
+bool StraightenByWall() {
+    int turnCounter;
+    int wallScan[21];
+    left(12);
+    for (turnCounter = 0; turnCounter <= 20; turnCounter++) {
+        stop();
+        right(1);
+        delay(100);
+        // read distance
+        wallScan[turnCounter] = distance;
+    }
+    // get max of wallScan if not over certain threshold bigger than average
+    left(21);
+    right(/* max index of wallScan*/);
+    return true;
 }
